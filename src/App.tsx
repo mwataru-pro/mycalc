5660d8e57e05c439c1cf038e25ff5b77f210e594
W.Mochizuki <m1130wata@gmail.com>
2023-02-08 23:29:30 +0900

    [add] first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Calculator from './component/Calculator'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  )
+}
+
+export default App
